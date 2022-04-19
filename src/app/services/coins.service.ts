cdc485621fb8ba2f7c9b0dd26b3d71d858900f5f
InNoobWeTrust <vu.truong.16193@gmail.com>
2022-04-19 01:20:20 +0000

    feat: coins service

--- src/app/services/coins.service.ts
+++ src/app/services/coins.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+const COINS_API = 'https://api.coingecko.com/api/v3/coins/';
+const COINS_API_ROUTES = {
+  list: 'list',
+};
+
+interface Coin {
+  id: string;
+  symbol: string;
+  name: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoinsService {
+  private _coinsSubject$: BehaviorSubject<Coin[]> = new BehaviorSubject<Coin[]>(
+    []
+  );
+  readonly coins$: Observable<Coin[]> = this._coinsSubject$.asObservable();
 
-  constructor() { }
+  constructor(private _http: HttpClient) {
+    this.refreshList();
+  }
+
+  refreshList() {
+    this._http
+      .get<Coin[]>(`${COINS_API}${COINS_API_ROUTES.list}`)
+      .subscribe((coins: Coin[]) => this._coinsSubject$.next(coins));
+  }
 }
--- src/app/services/exchange-rates.service.ts
+++ src/app/services/exchange-rates.service.ts
@@ -5,29 +5,32 @@ import { BehaviorSubject, Observable } from 'rxjs';
 const EXCHANGE_API = 'https://api.coingecko.com/api/v3/exchange_rates';
 
 interface Exchange {
-  name: string,
-  unit: string,
-  value: number,
-  type: string,
+  name: string;
+  unit: string;
+  value: number;
+  type: string;
 }
 
 interface ExchangeRates {
-  rates: Record<string, Exchange>,
+  rates: Record<string, Exchange>;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ExchangeRatesService {
-  private rateSubject$: BehaviorSubject<ExchangeRates> = new BehaviorSubject<ExchangeRates>({rates: {}});
-  rates$: Observable<ExchangeRates> = this.rateSubject$.asObservable();
+  private _rateSubject$: BehaviorSubject<ExchangeRates> =
+    new BehaviorSubject<ExchangeRates>({ rates: {} });
+  readonly rates$: Observable<ExchangeRates> =
+    this._rateSubject$.asObservable();
 
   constructor(private _http: HttpClient) {
     this.refresh();
   }
 
   refresh() {
-    this._http.get<ExchangeRates>(EXCHANGE_API).subscribe((rates: ExchangeRates) => (this.rateSubject$.next(rates)));
-
+    this._http
+      .get<ExchangeRates>(EXCHANGE_API)
+      .subscribe((rates: ExchangeRates) => this._rateSubject$.next(rates));
   }
 }
